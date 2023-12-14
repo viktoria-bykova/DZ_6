0a0ec1d221df57af0a75f7283127ed4021bb8fd2
Viktoria Bykova <viktoribox@gmail.com>
2023-12-15 00:08:10 +0300

    DZ_6

---
+++ Task_02/Program.cs
@@ -0,0 +1,20 @@
+﻿// Задача 2: Задайте строку, содержащую латинские буквы в обоих регистрах. 
+// Сформируйте строку, в которой все заглавные буквы заменены на строчные.
+// “aBcD1ef!-” => “abcd1ef!-” 
+
+string ConvertToLower(string input)
+{
+    char[] characters = input.ToCharArray();
+    for (int i = 0; i < characters.Length; i++)
+    {
+        if (characters[i] >= 'A' && characters[i] <= 'Z')
+        {
+            characters[i] = (char)(characters[i] + 32);
+        }
+    }
+    return new string(characters);
+}
+
+string sourceString = "dOcdECVEsKLeGqaulimWzDlxflRoAgGb";
+string resultString = ConvertToLower(sourceString);
+Console.WriteLine("Результирующая строка: " + resultString);
